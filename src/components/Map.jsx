41bb5372db2b983df81054c31eae6c62230fb616
greatHannahV <i.kasl@mail.ru>
2024-01-11 17:37:00 +0200

    nr

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,6 +1,6 @@
 import styles from './Map.module.css'
 function Map() {
-  return <div className={styles.mapContainer}>Map</div>
+  return <div className={styles.mapContainer}></div>
 }
 
 export default Map
