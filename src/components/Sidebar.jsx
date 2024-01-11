effce99b1763273cb9cb4612950e91c13fe0c181
greatHannahV <i.kasl@mail.ru>
2024-01-11 21:04:04 +0200

    +12

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,6 +9,7 @@ function Sidebar() {
       <Logo />
       <AppNav />
       <Outlet />
+
       <footer className={styles.footer}>
         <p className={styles.copyright}>
           &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
