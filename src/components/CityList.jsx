7aa0287e4a41526f707cf4085f62821e2d6de196
greatHannahV <i.kasl@mail.ru>
2024-01-12 17:38:53 +0200

    state as url part

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,6 +1,7 @@
 import CityItem from './CityItem'
 import styles from './CityList.module.css'
 import Spinner from './Spinner'
+import Message from './Message'
 function CityList({ cities, isLoading }) {
   if (isLoading) return <Spinner />
   if (!cities.length)
